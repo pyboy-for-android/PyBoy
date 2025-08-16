8c529b60aa6abbdcd2aeda627c3530f8b2ea3f91
Sergio Melero <sergiomele97@gmail.com>
2025-08-16 19:58:33 +0200

    Prueba en VRAM1

--- pyboy/core/lcd.py
+++ pyboy/core/lcd.py
@@ -872,6 +872,7 @@ class CGBRenderer(Renderer):
         self.clear_cache()
 
     def _cgb_get_background_map_attributes(self, lcd, i):
+        i = i % VIDEO_RAM
         tile_num = lcd.VRAM1[i]
         palette = tile_num & 0b111
         vbank = (tile_num >> 3) & 1
